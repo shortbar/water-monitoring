a8f680031b662f457047f7eaf856cd6f8f624b01
Patrick Souza <souza.patrick@gmail.com>
2013-02-16 19:32:23 -0800

    Added tests for the parser in stateread.py and created framework for overall project structure

--- stateread.py
+++ stateread.py
@@ -1,13 +1,46 @@
+# stateready.py
+# Patrick Souza <souza.patrick@gmail.com> for Prefiat LLC
+# Opens system state file and parses parameters for operation.
+
 import yaml
+import states
+
+class InvalidStateFileError(Exception):
+    pass 
+
+class MissingStateFileError(Exception):
+    pass
 
 def get_file_handle(state_file):
-    f = open(state_file, 'r')
-    return f
+    try:
+        f = open(state_file, 'r')
+        return f
+    except IOError:
+        raise MissingStateFileError()
 
 def parse_state_file(state_file):
     saved_state_file = get_file_handle(state_file)
-    state_info = yaml.load(saved_state_file)
-    return state_info
+            
+    try:
+        state_info = yaml.load(saved_state_file)
+        
+        since = state_info["since_local"]
+        message = state_info["message"]
+    
+        if state_info["state"] == "okay":
+            current_state = states.OKState(since, message)
+        elif state_info["state"] == "warning":
+            current_state = states.WarningState(since, message)
+        elif state_info["state"] == "action":
+            current_state = states.ActionState(since, message)
+    except:
+        raise InvalidStateFileError()
+    
+    return current_state
+
+# ToDo: Raise exception if type(since) != datetime
+
+
 
     
     
--- test/test_stateread.py
+++ test/test_stateread.py
@@ -1,9 +1,13 @@
+# test_stateread.py
+# Patrick Souza <souza.patrick@gmail.com> for Prefiat LLC
+# Test methods for the statread.py method: reading system state.
+
 import unittest
 import stateread
 import yaml
 import os
 import datetime
-
+import states
 
 class TestFileFind(unittest.TestCase):
     def setUp(self):
@@ -11,38 +15,60 @@ class TestFileFind(unittest.TestCase):
         #Create mock files for testing
         self.mock_file_dict = {"empty_mock.yaml" : "",
                                "normal_mock.yaml" :    """---\n
-                                                       State : Okay\n
-                                                       Timestamp : !!timestamp '2013-02-10 13:12:11'\n
-                                                       Message : No Message"""
-                               }
+                                                       state : okay\n
+                                                       since_local : !!timestamp '2013-02-10 13:12:11'\n
+                                                       message : No Message""",
+                               "warning_mock.yaml":    """---\n
+                                                      state : warning\n
+                                                      since_local : !!timestamp '2013-02-15 13:12:11'\n
+                                                      message : No Message""",
+                               "action_mock.yaml":    """---\n
+                                                      state : action\n
+                                                      since_local : !!timestamp '2013-02-28 13:12:11'\n
+                                                      message : No Message""",
+                                "trash_mock.yaml":   """a;slkdfj;laskjdf;akj""",
+                                
+                                }
         for i in self.mock_file_dict.iterkeys():
             f = open(i, "w+")
             f.write(self.mock_file_dict[i])
             f.close                
-        
-    
+                    
     def test_FileMissing(self):
-        self.assertRaises(IOError, lambda: stateread.get_file_handle("nonexistent.yaml"))
+        self.assertRaises(stateread.MissingStateFileError, stateread.get_file_handle, "nonexistent.yaml")
         
     def test_FileNotMissing(self):  
         actual = stateread.get_file_handle("empty_mock.yaml")
         self.assertIs(type(actual), file, "File is missing")
+        
+    def test_Recovers_OK_State(self):
+        actual = stateread.parse_state_file("normal_mock.yaml")
+        self.assertIsInstance(actual, states.OKState, "Error Recovering OK State")
+        
+    def test_Recovers_Warning_State(self):
+        actual = stateread.parse_state_file("warning_mock.yaml")
+        self.assertIsInstance(actual, states.WarningState, "Error Recovering Warning State")
     
-    def test_Parse_Parameters(self):
-        actual_state = stateread.parse_state_file("normal_mock.yaml")
-        for i in actual_state.iterkeys():
-            self.assertIn(i, ["State", "Timestamp", "Message"], "Invalid State Keys")
-        self.assertIn(actual_state["State"], ["Okay","Warning","Alert"], "Error reading system state")
-        self.assertIs(type(actual_state["Timestamp"]), datetime.datetime, "Error reading datetime")
-        self.assertIs(type(actual_state["Message"]), str, "Error reading string")
+    def test_Recovers_Alert_State(self):
+        actual = stateread.parse_state_file("action_mock.yaml")
+        self.assertIsInstance(actual, states.ActionState, "Error Recovering Action State")
     
+    def test_Recovers_correct_date(self):
+        actual = stateread.parse_state_file("normal_mock.yaml").since
+        expected = datetime.datetime(2013, 2, 10, 13, 12, 11)
+        self.assertEqual(actual, expected, "Unexpected datetime")
+    
+    def test_Recovers_correct_message(self):
+        actual = stateread.parse_state_file("normal_mock.yaml").message
+        expected = "No Message"
+        self.assertEqual(actual, expected, "Unexpected message paramater")
+    
+    def test_Raises_Trash_Exception(self):
+        self.assertRaises(stateread.InvalidStateFileError, stateread.parse_state_file, "trash_mock.yaml")
+        
     def tearDown(self):
         for i in self.mock_file_dict.iterkeys():
             os.remove(i)
         
-            
-        
-        
 if __name__ == '__main__':
     unittest.main()
-        
