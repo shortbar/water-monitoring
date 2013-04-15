4551ca4cb43593bf1b828410bdf2a0a1814772ad
Patrick Souza <souza.patrick@gmail.com>
2013-04-15 09:24:28 -0700

    Added transition rules for Warning and Action states along with tests

--- states.py
+++ states.py
@@ -27,10 +27,14 @@ class OKState(SystemState):
     def next(self, gpio_mgr, api_mgr):
         fs_state = gpio_mgr.is_floating()
         api_state = api_mgr.read_api()
-        if api_state < 15.3:
+        d_time = (datetime.datetime.now() - self.since).total_seconds()
+        if d_time < 3600:
+            return OKState(datetime.datetime.now(), "Current State is: Okay. Plum Creek water level at {} meters".format(api_state))
+        elif api_state < 15.3:
             return OKState(datetime.datetime.now(), "Current State is: Okay. Plum Creek water level at {} meters".format(api_state))
         elif api_state >= 15.3:
             return WarningState(datetime.datetime.now(), "Current state is: Warning. Plum Creek water level at {} meters".format(api_state))
+        
     def set_outputs(self, gpio_mgr):
         return gpio_mgr.set_OKState()
 
@@ -38,7 +42,16 @@ class WarningState(SystemState):
     def next(self, gpio_mgr, api_mgr):
         fs_state = gpio_mgr.is_floating()
         api_state = api_mgr.read_api()
-        pass
+        d_time = (datetime.datetime.now() - self.since).total_seconds()
+        if d_time < 900:
+            return WarningState(datetime.datetime.now(), "Current State is: Warning. Plum Creek water level at {} meters".format(api_state))
+        elif fs_state == True and api_state >= 15.3:
+            return ActionState(datetime.datetime.now(), "Current State is: Action. Float switch is either raised or out of order. Plum Creek water level at {} meters".format(api_state))
+        elif fs_state == False and api_state >= 15.3:
+            return WarningState(datetime.datetime.now(), "Current State is: Warning. Plum Creek water level at {} meters".format(api_state))
+        elif api_state < 15.3:
+            return OKState(datetime.datetime.now(), "Current State is: Okay. Plum Creek water level at {} meters".format(api_state))
+        
     def set_outputs(self, gpio_mgr):
         return gpio_mgr.set_WarningState()
 
@@ -54,7 +67,13 @@ class ActionState(SystemState):
     def next(self, gpio_mgr, api_mgr):
         fs_state = gpio_mgr.is_floating()
         api_state = api_mgr.read_api()
-        pass
+        d_time = (datetime.datetime.now() - self.since).total_seconds()
+        if d_time < 900:
+            return ActionState(datetime.datetime.now(), "Current State is: Action. Plum Creek water level at {} meters. Float switch raised: {}".format(api_state, fs_state))
+        elif api_state >= 15:
+            return ActionState(datetime.datetime.now(), "Current State is: Action. Plum Creek water level at {} meters. Float switch raised: {}".format(api_state, fs_state))
+        elif api_state < 15:
+            return OKState(datetime.datetime.now(), "Current State is: Okay. Plum Creek water level at {} meters".format(api_state))
     def set_outputs(self, gpio_mgr):
         return gpio_mgr.set_ActionState()
 
--- test/test_nextmethods.py
+++ test/test_nextmethods.py
@@ -8,22 +8,93 @@ import gpiomanager
 import apifake
 import pifake
 
-class testOKStateTransitions(unittest.TestCase):
+class TestOKStateTransitions(unittest.TestCase):
     
     def setUp(self):
         self.api_mgr = apifake.api_fake()
         self.pfio_interface_fake = pifake.pfio_fake()
         self.gpio_mgr = gpiomanager.GPIOManager(self.pfio_interface_fake)
     
+    def tearDown(self):
+        pass
+    
     def test_OKState_to_OKState(self):
         self.api_mgr.water_level = 10
-        current_state = states.OKState(datetime.datetime.now(), "foo")
+        current_state = states.OKState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
         current_state = current_state.next(self.gpio_mgr, self.api_mgr)
         self.assertIsInstance(current_state, states.OKState, "Error maintaining OKState")
         
     def test_OKState_to_WarningState(self):
         self.api_mgr.water_level = 16
-        current_state = states.OKState(datetime.datetime.now(), "foo")
+        current_state = states.OKState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
         current_state = current_state.next(self.gpio_mgr, self.api_mgr)
         self.assertIsInstance(current_state, states.WarningState, "Error in OKState -> WarningState transition")
         
+    def test_BelowTimeThreshold(self):
+        self.api_mgr.water_level = 16
+        current_state = states.OKState(datetime.datetime.now() - datetime.timedelta(0, 60, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.OKState, "Error in maintaining OKState by insufficient time interval")
+
+class TestWarningStateTransitions(unittest.TestCase):
+    
+    def setUp(self):
+        self.api_mgr = apifake.api_fake()
+        self.pfio_interface_fake = pifake.pfio_fake()
+        self.gpio_mgr = gpiomanager.GPIOManager(self.pfio_interface_fake)
+        
+    def test_WarningState_to_WarningState(self):
+        self.api_mgr.water_level = 16
+        self.pfio_interface_fake.inputs[0] = 1
+        current_state = states.WarningState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.WarningState, "Error maintaining Warning State") 
+    
+    def test_WarningState_to_OKState(self):
+        self.api_mgr.water_level = 10
+        self.pfio_interface_fake.inputs[0] = 1
+        current_state = states.WarningState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.OKState, "Error in WarningState -> OKState transition")
+        
+    def test_WarningState_to_ActionState(self):
+        self.api_mgr.water_level = 16
+        self.pfio_interface_fake.inputs[0] = 0
+        current_state = states.WarningState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.ActionState, "Error in WarningState -> ActionState transition")
+        
+    def test_BelowTimeThreshold(self):
+        self.api_mgr.water_level = 16
+        self.pfio_interface_fake.inputs[0] = 0
+        current_state = states.WarningState(datetime.datetime.now() - datetime.timedelta(0, 400, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.WarningState, "Error in time difference check.")              
+
+class TestActionStateTransitions(unittest.TestCase):
+    
+    def setUp(self):
+        self.api_mgr = apifake.api_fake()
+        self.pfio_interface_fake = pifake.pfio_fake()
+        self.gpio_mgr = gpiomanager.GPIOManager(self.pfio_interface_fake)
+        
+    def test_ActionState_to_ActionState(self):
+        self.api_mgr.water_level = 16
+        self.pfio_interface_fake.inputs[0] = 0
+        current_state = states.ActionState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.ActionState, "Error maintaining Action State")
+    
+    def test_ActionState_to_OKState(self):
+        self.api_mgr.water_level = 10
+        self.pfio_interface_fake.inputs[0] = 0
+        current_state = states.ActionState(datetime.datetime.now() - datetime.timedelta(0, 4000, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.OKState, "Error in ActionState -> OKState transition")
+    
+    def test_BelowTimeThreshold(self):
+        self.api_mgr.water_level = 10
+        self.pfio_interface_fake.inputs[0] = 0
+        current_state = states.ActionState(datetime.datetime.now() - datetime.timedelta(0, 400, 0), "foo")
+        current_state = current_state.next(self.gpio_mgr, self.api_mgr)
+        self.assertIsInstance(current_state, states.ActionState, "Error in timedelta check")
