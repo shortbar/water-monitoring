273f55713d7db10d7026c786768c244e659cc95d
David R. Albrecht <albrecht.dr@gmail.com>
2013-06-05 22:25:54 -0700

    Fixed tabs in main

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ gpio_mgr = gpiomanager.GPIOManager(pfio_interface)
 gis_api = apimanager.GISAPIManager()
 
 try:
-	current_state = statemanager.parse_state_file("state_file.yaml")
-	current_state = current_state.next(gpio_mgr, gis_api)
+    current_state = statemanager.parse_state_file("state_file.yaml")
+    current_state = current_state.next(gpio_mgr, gis_api)
 except:
-	current_state = states.TroubleState(datetime.datetime.now(), sys.exc_info()[0])
+    current_state = states.TroubleState(datetime.datetime.now(), sys.exc_info()[0])
 finally:
-	current_state.set_outputs(gpio_mgr)
-    current_state.dumpconsole()
-	statemanager.write_state_file("state_file.yaml", current_state)
+    current_state.set_outputs(gpio_mgr)
+    statemanager.write_state_stdout(current_state)
+    statemanager.write_state_file("state_file.yaml", current_state)
