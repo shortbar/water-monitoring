df5179faaebd87ff28ce4b2f220836f778d130c5
ugurthemaster <ugurthemaster@gmail.com>
2013-08-23 09:23:08 +0300

    tabs replace with 4 spaces per indentation level (As advised in PEP 8).

--- scripts/outputtest.py
+++ scripts/outputtest.py
@@ -1,13 +1,15 @@
 import piface.pfio as pfio
 from time import sleep
+
 pfio.init()
 out_list = [1, 2, 3, 4]
 print "Press Ctrl-C to end"
+
 try:
-	while True:
-		for i in range(4):
-			pfio.digital_write(out_list[i-1],0)
-			pfio.digital_write(out_list[i],1)
-			sleep(1)
+    while True:
+        for i in range(4):
+            pfio.digital_write(out_list[i-1],0)
+            pfio.digital_write(out_list[i],1)
+            sleep(1)
 except:
-	pfio.init()
+    pfio.init()
